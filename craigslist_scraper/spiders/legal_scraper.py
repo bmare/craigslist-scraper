28bfdc2e884a7bb3eff84da877a98121c0994537
bmare <brandon.marrow@gmail.com>
2019-08-27 09:55:06 -0400

    added git ignore; removed .pyc files

--- craigslist_scraper/spiders/legal_scraper.py
+++ craigslist_scraper/spiders/legal_scraper.py
@@ -6,16 +6,16 @@ class CLSpider( scrapy.Spider ):
     start_urls = [
                 'https://boston.craigslist.org/search/gbs/lgl',
     ]
-    def parse ( self, response):
-        for job in response.css('a.result-title.hdrlnk'):
-            yield {
-                    'title': job.css('::text').get(),
-                    'link': job.css('::attr(href)').get(),
-            }
+#    def parse ( self, response):
+#        for job in response.css('a.result-title.hdrlnk'):
+#            yield {
+#                    'title': job.css('::text').get(),
+#                    'link': job.css('::attr(href)').get(),
+#            }
 
 
-    #def parse ( self, response ):
-        #filename="clegal_jobs.txt"
-        #with open(filename, 'wb') as f:
-        #    for job in response.css('a.result-title.hdrlnk'):
-        #        f.write(job.css('::text').get())
+    def parse ( self, response ):
+        filename="clegal_jobs.txt"
+        with open(filename, 'wb') as f:
+            for job in response.css('a.result-title.hdrlnk'):
+                f.write(job.css('::text').get())
